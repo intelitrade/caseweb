fa5534df342c1b7efd354a54b30f428c16520f7c
Chim <chimwemwe@gmail.com>
2019-02-03 20:16:52 +0200

    Add files via upload

--- cwwebservice.js
+++ cwwebservice.js
@@ -2,5 +2,21 @@ function mytest1(oDoc)
 {
 	debugger;
 	debugger;
-	alert(oDoc.cellCount());
+	//alert(oDoc.cellCount());
+	var iSectionCount = oDoc.sectionCount();
+	var sPages = ""
+	for(var i=1;i<=iSecionCount;i++)
+	{
+		var oSection = oDoc.section(i);
+		if(oSection && oSection.propExists("CFORMAT")==1)
+		{
+			if(sPages=="")
+			{
+				sPages = "<li>"+oSection.label+"<li>";
+			}else{
+				sPages = sPages + "<li>"+oSection.label+"</li>";
+			}
+		}
+	}
+	 document.getElementById("demo").innerHTML = "<ul>"+sPages+"</ul>";
 }
